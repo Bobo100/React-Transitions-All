60d5db2abb20c632e6e832f39822debc98a83633
Bobo100 <w150lione@gmail.com>
2023-02-07 23:03:52 +0800

    更新404頁面

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Flip } from './component/Flip';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import NotFoundPage from './pages/Error';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/React-Transitions-All" element={<Home />} />
           <Route path="/book" element={<Book />} />
           <Route path="/flip" element={<Flip />} />
+          <Route path="*" element={<NotFoundPage />}></Route>
         </Routes>
       </Router>
     </>
